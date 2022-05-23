58b51c8338b261088ef3a4eb05b2e76092ae1f25
djpeela <djpeela24@gmail.com>
2022-05-23 22:53:35 +0530

    first commit

---
+++ javapractice.js
@@ -0,0 +1,250 @@
+// const wish = (' good morning ')
+// const name = ('jagan')
+
+// console.log( ' hi ' + wish + ' this is ' + name);
+
+// let x = 4;
+// let y = 5;
+
+// console.log(x+y);
+
+// function numbers (x, y) {
+//     console.log(x+y);
+// }
+
+// numbers(5,6)
+
+// var profile = {
+//     name : 'jagan',
+//     lastname : 'peela',
+//     age : 28,
+//     address : {
+//         street : 'krsishnalayam',
+//         pin : 530047
+//     }
+// }
+
+// console.log(profile.address.pin);
+
+// var members = [
+//     person1 : {
+//         name : 'jagan',
+//         age : 24
+//     }
+
+//     person2 = {
+//         name : 'peela',
+//         age : 28
+//     }
+
+//     person3 : {
+//         name : 'dimbu',
+//         age : 32
+//     }
+// ]
+
+// console.log(members.20<Array.age);
+
+// const number = [1, 2, 3, 4, 5];
+
+// number.map((sum) => console.log(sum + 10));
+
+// const person = [
+//     {
+//         name : 'manu',
+//         age : 24,
+//         gender : 'male'
+//     },
+//     {
+//         name : 'jagan',
+//         age : 28,
+//         gender : 'male'
+//     },
+//     {
+//         name : 'tanuja',
+//         age : 22,
+//         gender : 'female'
+//     },
+//     {
+//         name : 'yakshana',
+//         age : 26,
+//         gender : 'female'
+//     }
+// ];
+
+// const personText = person.filter(function(person) {
+//     return person.age > 25 ;
+// });
+
+// console.log(personText);
+
+// const wish = 'HELLO GOOD MORNING';
+
+// console.log(wish.toLowerCase());
+
+// var numbers = [6, 12, 18, 24];
+// result = {};
+//     for (i = 0; i >= numbers.length; i++) {
+//     result = {} + numbers[i];
+//     result = 5 + result/6 *2;
+// };
+
+// console.log(result);
+
+// const number = 5;
+
+// for (let i = number; i > 0; i--) {
+//     var result = '';
+//     for (let j = 0; j < i; j++){
+//         result = '*' + result;
+//     }
+//     console.log(result);
+// };
+
+// const number = 5;
+
+// result = '*';
+
+// for (let i = 0; i < number; i++) {
+//     var result = '';
+//     for (let j = 0; j > i; j++){
+//         result = '' + result;
+//     }
+//     console.log(result);
+// };
+
+// even numbers
+// const number = 15;
+
+// for (i = 0; i <= number; i++) {
+//     if (i%2 == 0 ){
+//         console.log(i);
+//     }
+// }
+
+// odd numbers
+// const number = 15;
+
+// for (i = 0; i <= number; i++) {
+//     if (i%2 != 0 ){
+//         console.log(i);
+//     }
+// }
+
+// prime numbers
+// const number = 25;
+
+// for (i = 1; i <= number; i++) {
+//   var count = 0;
+//   for (j = 1; j <= i; j++) {
+//     if (i % j == 0) {
+//       count = count + 1;
+//     }
+//   }
+
+//   if (count == 2) {
+//     console.log(i);
+//   }
+// };
+
+// const number = 25;
+
+// result = [];
+// for (i = 1; i <= number; i++) {
+//   var count = 0;
+//   for (j = 1; j <= i; j++) {
+//     if (i % j == 0) {
+//       count = count + 1;
+//     }
+//   }
+
+//   if (count == 2) {
+//     result.push(i);
+//   }
+// }
+
+// console.log(result);
+
+// const companies = [
+//     { name: "Company 1", category: "finance", start: 1981, end: 2003 },
+//     { name: "Company 2", category: "retail", start: 1992, end: 2008 },
+//     { name: "Company 3", category: "auto", start: 1999, end: 2007 },
+//     { name: "Company 4", category: "retail", start: 1989, end: 2010 },
+//     { name: "Company 5", category: "technology", start: 2009, end: 2014 },
+//     { name: "Company 6", category: "finance", start: 1987, end: 2010 },
+//     { name: "Company 7", category: "auto", start: 1986, end: 1996 },
+//     { name: "Company 8", category: "technology", start: 2011, end: 2016 },
+//     { name: "Company 9", category: "retail", start: 1981, end: 1989 }
+//   ];
+
+
+//   for (i = 0; i <= companies.length; i++) {
+    
+//       console.log(companies[i].start);
+//   };
+
+
+// finding array lenght 
+// const number = [1, 2, 3, 4, 5, 6]
+
+// var l = 0;
+
+// while (number[l]) {
+//     l = l + 1;
+// }
+
+// console.log(l);
+
+
+// function factorial(n) {
+//     if (n === 0) {
+//         return 1;
+//     }
+//     return n * factorial(n - 1);
+// }
+
+// console.log(factorial(5));
+
+
+// function factorial(n) {
+//     var result = ''
+//     var ans = 1;
+//     for (i = 1; i <= n; i++) {
+//         var l = i + 'x';
+//         ans = ans * i;
+//         result = result + l;
+//     }
+//     return result + ' is ' +ans;
+// };
+
+// console.log(factorial(4));
+
+// console.log(factorial(9));
+
+// function factorial(n) {
+//     var result = ''
+//     var ans = 1;
+//     for (i = 1; i <= n; i++) {
+//         var l = i + 'x';
+//         if(i == n) {
+//             var l = i;
+//         }
+//         ans = ans * i;
+//         result = result + l;
+//     }
+//     return result + ' is ' +ans;
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
